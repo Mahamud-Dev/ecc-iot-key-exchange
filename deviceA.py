e51e5a7158fca118b4ca4f8b4356c0e3cde8afbb
Shuaib <s.mahamud17@outlook.com>
2025-06-05 13:39:03 +0100

    deviceA.py

--- deviceA.py
+++ deviceA.py
@@ -3,22 +3,18 @@ from cryptography.hazmat.primitives.asymmetric import ec
 
 
 
-# 2. Derive the public key from the private key
-# public_key = private_key.public_key()
-
-
-# 5. Load Device A's private key
+# Load Device A's private key
 with open("device_a_private.pem" , "rb") as f:
     private_key = serialization.load_pem_private_key(
         f.read(),
         password=None
     )
 
-#6. Load Device B's public key
+# Load Device B's public key
 with open("device_b_public.pem" , "rb") as f:
     public_key = serialization.load_pem_public_key(f.read())
 
-#7. Derive the shared key
+# Derive the shared key
 shared_key_a = private_key.exchange(ec.ECDH() , public_key)
 
 print("Device A Shared Key:", shared_key_a.hex())
